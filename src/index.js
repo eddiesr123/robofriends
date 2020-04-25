822f9d89b1164d0f4f57f8163448aea70ef3f856
eddiesr123 <eddiesr1234@gmail.com>
2020-04-25 01:27:34 -0700

    added redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -10,7 +10,8 @@ import 'tachyons';
 import App from './containers/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
